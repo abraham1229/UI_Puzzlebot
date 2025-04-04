3be8051ed0a8f08508fe2c19d1e2d5231603a6cd
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-04 12:24:51 -0500

    Layout added

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+
 export default function Layout() {
   return (
-    <div>
-      <h1>From layout</h1>
-      <Outlet />
+    <div className='bg-slate-950 min-h-screen text-white pt-15'>
+      <div className='flex justify-evenly items-baseline'>
+        <h1 className='text-3xl'> URDF Visualizer for Rviz2</h1>
+        <p> Conexion establecida </p>
+      </div>
+      <div className='m-16'>
+        <Outlet />
+      </div>
       
     </div>
   )
--- frontend/src/view/IndexView.jsx
+++ frontend/src/view/IndexView.jsx
@@ -1,6 +1,13 @@
 
 export default function IndexView() {
   return (
-    <div>IndexView</div>
+    <div className="grid grid-cols-3 gap-10 h-[80vh]">
+      <div className="border-2 rounded-xl p-10">
+        <h2 className="text-2xl"> Model Selection </h2>
+      </div>      
+      <div className="col-span-2 rounded-xl border-2 p-10">
+        <h2 className="text-2xl"> Model Information</h2>
+      </div>        
+    </div>
   )
 }
