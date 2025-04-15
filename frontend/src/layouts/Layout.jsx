4a4f6e93619b4048fa0159a69784320e278ed6e7
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-15 09:01:44 -0500

    Se agregan iconos

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { useTopicStatus } from '../hooks/connection'
+import { CheckCircleIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
 
 export default function Layout() {
 
@@ -10,11 +11,19 @@ export default function Layout() {
     <div className='bg-slate-950 min-h-screen text-white pt-15'>
       <div className='flex flex-col text-center lg:flex-row lg:justify-evenly lg:items-baseline'>
         <h1 className='text-3xl'> URDF Visualizer for Rviz2</h1>
-        <p className='text-xl mt-5'> 
-          {conectionStatus ? 
-            'Conexión establecida mediante web socket' : 
-            'Estableciendo conexion...'} 
-          </p>
+        <p className='text-xl mt-5 flex items-center justify-center gap-3'>
+          {conectionStatus ? (
+            <>
+              <CheckCircleIcon className='h-6 w-6 text-green-500' />
+              Conexión establecida mediante WebSocket
+            </>
+          ) : (
+            <>
+              <ArrowPathIcon className='h-6 w-6 text-yellow-400 animate-spin' />
+              Estableciendo conexión...
+            </>
+          )}
+        </p>
       </div>
       <div className='m-16'>
         <Outlet />
